71d91c5065071db094fe64ccc0a04ce61a976c39
Jalilov <javohirjalilov777@gmail.com>
2022-02-22 00:59:32 +0500

    download model

---
+++ down_model.sh
@@ -0,0 +1,7 @@
+# Model
+wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2017_11_17.tar.gz
+
+tar -xf ssd_mobilenet_v1_coco_2017_11_17.tar.gz
+
+#Config
+wget https://raw.githubusercontent.com/opencv/opencv_extra/master/testdata/dnn/ssd_mobilenet_v1_coco_2017_11_17.pbtxt
