f04be24d61d395b6deb8dabda6562692d254a2d3
Jalilov <javohirjalilov777@gmail.com>
2022-02-22 09:05:44 +0500

    object detection

--- object_detection.py
+++ object_detection.py
@@ -1,8 +1,26 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import main
 
+frame = main.read_image('data/data_img.jpg')
+rows, cols, channels = frame.shape
 
-bufferModel = 'ssd_mobilenet_v1_coco_2017_11_17/frozen_inference_graph.pb'
-bufferConfig = 'ssd_mobilenet_v1_coco_2017_11_17.pbtxt'
+networkOutput = main.prediction(frame)
 
-tensorflowNet = cv2.dnn.readNetFromTensorflow(bufferModel, bufferConfig)
+for detection in networkOutput[0,0]:
+        
+    score = float(detection[2])
+    if score > 0.2:
+        
+        left = detection[3] * cols
+        top = detection[4] * rows
+        right = detection[5] * cols
+        bottom = detection[6] * rows
+
+        #draw a red rectangle around detected objects
+        cv2.rectangle(frame, (int(left), int(top)), (int(right), int(bottom)), (0, 0, 255), thickness=2)
+
+cv2.imwrite("predict_data/prediction.jpg", frame)
+plt.imshow(frame)
+plt.show()
